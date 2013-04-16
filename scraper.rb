59982a57b0119093b361e39dc109e0ffee4a0c02
Kenny Chan <kenny.r.chan@gmail.com>
2013-04-15 22:47:46 -0400

    add logging to scraper

--- scraper.rb
+++ scraper.rb
@@ -7,22 +7,33 @@ require 'debugger'
 $skipExistingDays = false
 
 def pullDataFor(resort)
+	log = Logger.new('logs/scraper_log.txt')
+	log.level = Logger::WARN
+	log.error('Start parse')
+
 	years = ['2007', '2008', '2009', '2010', '2011', '2012', '2013']
 	resort_name = resort.name
 	years.each do |year|
 		uri = 'http://www.onthesnow.com/' + resort.state + '/' + resort_name + '/historical-snowfall.html?&y=' + year + '&q=base&v=list#view'
 		p 'opening nokogiri for ' + uri
+		log.error 'opening nokogiri for ' + uri
 		begin
 			doc = Nokogiri::HTML(open(uri))
 
 			#check for formatted resort name
 			if !resort.formatted_name
-				resort.update_attributes(formatted_name: doc.css('resort_name')[0].text)
+				if doc.css('.resort_name').length > 0
+					resort.update_attributes(formatted_name: doc.css('.resort_name')[0].text)
+				else
+					resort.update_attributes(formatted_name: resort.name)
+				end
 				p 'populating formatted report name: ' + resort.formatted_name
+				log.error 'populating formatted report name: ' + resort.formatted_name
 			end
 
 			days = doc.css('table.snowfall tr:not(.titleRow)')
 			days.each do |day|
+				# log.error day.inspect
 				cols = day.css('td')
 				date = Date.parse cols[0].text
 
@@ -49,6 +60,8 @@ def pullDataFor(resort)
 						precipitation: cols[1].text.match(/[0-9]+/)[0].to_i,
 						season_snow: cols[3].text.match(/[0-9]+/)[0].to_i
 					)
+					log.error 'new day created'
+					log.error new_day.inspect
 				else
 					if $skipExistingDays && (date.year != Time.now.year) #never skip if the date is this year, because we always want to update for most recent year. But we can ignore past years
 						p 'Existing snow day found for ' + resort_name + ': ' + date_string + ', skipping'
@@ -64,6 +77,8 @@ def pullDataFor(resort)
 							season_snow: cols[3].text.match(/[0-9]+/)[0].to_i,
 							generated: false
 						)
+						log.error 'existing day updated'
+						log.error existing_day.inspect
 					end
 				end
 			end
